70e4d47abad859f0ebe594605e9c460863ecd36c
dachus <dachus@dachus>
2023-10-19 15:36:45 +0300

    pinecone working

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-// import { Index, Pinecone } from "@pinecone-database/pinecone";
+import { Index, Pinecone } from "@pinecone-database/pinecone";
 // import { recommend } from './recommend';
 // import { Embedder } from "./embeddings";
 import dotenv from 'dotenv';
@@ -29,21 +29,21 @@ try {
   const port = 3000; // Choose your desired port
 
   const indexName = getEnv("PINECONE_INDEX");
-  // const pinecone = new Pinecone();
+  const pinecone = new Pinecone();
 
   app.get('/recommend/:query', async (req, res) => {
     const { query } = req.params;
-  //   try {
-  //     const description = await pinecone.describeIndex(indexName);
-  //     if (!description.status?.ready) {
-  //       throw `Index not ready, description was ${JSON.stringify(description)}`
-  //     }
-  //   } catch (e) {
-  //     console.log('An error occurred. Run "npm run index" to load data into the index before querying.');
-  //     throw e;
-  //   }
+    try {
+      const description = await pinecone.describeIndex(indexName);
+      if (!description.status?.ready) {
+        throw `Index not ready, description was ${JSON.stringify(description)}`
+      }
+    } catch (e) {
+      console.log('An error occurred. Run "npm run index" to load data into the index before querying.');
+      throw e;
+    }
     
-  //   const index: Index<ArticleRecord> = pinecone.index<ArticleRecord>(indexName).namespace('default');
+    const index: Index<ArticleRecord> = pinecone.index<ArticleRecord>(indexName).namespace('default');
   //   const embedder = new Embedder();
   //   await embedder.init("Xenova/all-MiniLM-L6-v2");
 
